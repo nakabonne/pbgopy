4c39fec065a599cf1d1fbc4a813449027ac320b5
Alexander Wilcots <acwilcots@gmail.com>
2020-12-01 15:21:13 -0600

    Issue #4 - Stop using an empty nonce for GCM
    In crypto/crypto.go
      Used crypto/rand to supply secure random data to fill the nonce
      Created func GetNonce to contain logic for generating the nonce
       given the nonce size
      Removed unused function RandomBytes. The functionality provided
       is simple to replicate if needed in the future. So for now, I'd
       just rather remove the cognative load of just having that there.
    
    In crypto/crypto_test.go
      Removed tests for RandomBytes

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -3,10 +3,10 @@ package crypto
 import (
 	"crypto/aes"
 	"crypto/cipher"
+	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
-	"math/rand"
-	"time"
+	"io"
 
 	"golang.org/x/crypto/pbkdf2"
 )
@@ -27,8 +27,11 @@ func Encrypt(password string, salt, data []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
-	nonce := make([]byte, gcm.NonceSize())
-	// TODO: Stop using an empty nonce for GCM
+	nonce, err := GetNonce(gcm.NonceSize())
+	if err != nil {
+		return nil, err
+	}
+
 	encryptedData := gcm.Seal(nonce, nonce, data, nil)
 	return encryptedData, nil
 }
@@ -54,16 +57,13 @@ func Decrypt(password string, salt, encryptedData []byte) ([]byte, error) {
 	return gcm.Open(nil, nonce, ciphertext, nil)
 }
 
-const charset = "abcdefghijklmnopqrstuvwxyz" +
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+func GetNonce(length int) ([]byte, error) {
+	nonce := make([]byte, length)
 
-var seededRand = rand.New(rand.NewSource(time.Now().UnixNano()))
-
-// RandomBytes yields a random bytes with the given length.
-func RandomBytes(length int) []byte {
-	b := make([]byte, length)
-	for i := range b {
-		b[i] = charset[seededRand.Intn(len(charset))]
+	_, err := io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
 	}
-	return b
+
+	return nonce, nil
 }
--- crypto/crypto_test.go
+++ crypto/crypto_test.go
@@ -1,7 +1,6 @@
 package crypto
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -45,26 +44,4 @@ func TestEncryptDecrypt(t *testing.T) {
 			assert.Equal(t, tt.wantSuccess, string(data) == string(plainText))
 		})
 	}
-}
-
-func TestRandomBytes(t *testing.T) {
-	validator := func(bs []byte) error {
-		for _, b := range bs {
-			if (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z') || (b >= '0' && b <= '9') {
-				continue
-			}
-			return fmt.Errorf("invalid character: %#U", b)
-		}
-		return nil
-	}
-
-	s1 := RandomBytes(10)
-	assert.Equal(t, 10, len(s1))
-	assert.NoError(t, validator(s1))
-
-	s2 := RandomBytes(10)
-	assert.Equal(t, 10, len(s2))
-	assert.NoError(t, validator(s2))
-
-	assert.NotEqual(t, s1, s2)
 }
