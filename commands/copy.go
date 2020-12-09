76dd4eaef4290aabcb74150c0b094e7b69f0c95f
Faithfulness Alamu <vaguemail369@gmail.com>
2020-12-09 20:43:11 +0100

    Allow setting --max-size on clipboard source
    When reading from clipboard, allow the user decide
    what the max-size should be.

--- commands/copy.go
+++ commands/copy.go
@@ -53,24 +53,25 @@ func (r *copyRunner) run(_ *cobra.Command, _ []string) error {
 		return fmt.Errorf("put the pbgopy server's address into %s environment variable", pbgopyServerEnv)
 	}
 
-	var data []byte
+	var source io.Reader
 
 	if r.fromClipboard {
 		clipboardData, err := clipboard.ReadAll()
 		if err != nil {
 			return err
 		}
-		data = []byte(clipboardData)
-
+		source = strings.NewReader(clipboardData)
 	} else {
-		sizeInBytes, err := datasizeToBytes(r.maxBufSize)
-		if err != nil {
-			return fmt.Errorf("failed to parse data size: %w", err)
-		}
-		data, err = readNoMoreThan(os.Stdin, sizeInBytes)
-		if err != nil {
-			return fmt.Errorf("failed to read from STDIN: %w", err)
-		}
+		source = os.Stdin
+	}
+
+	sizeInBytes, err := datasizeToBytes(r.maxBufSize)
+	if err != nil {
+		return fmt.Errorf("failed to parse data size: %w", err)
+	}
+	data, err := readNoMoreThan(source, sizeInBytes)
+	if err != nil {
+		return fmt.Errorf("failed to read from source: %w", err)
 	}
 
 	client := &http.Client{
